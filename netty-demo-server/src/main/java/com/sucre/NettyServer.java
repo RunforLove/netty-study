b308234e4b04e951929963b547de9c1809adf716
shuanghun <shuanghun@wacai.com>
2018-03-26 10:59:55 +0800

    first version

---
+++ netty-demo-server/src/main/java/com/sucre/NettyServer.java
@@ -0,0 +1,13 @@
+package com.sucre;
+
+/**
+ * Hello world!
+ *
+ */
+public class NettyServer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
